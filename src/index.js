81755755f7531dcc9f09e7c3b90be404af162305
Milad pegah <miladpegah@amoozesh-boors.com>
2023-09-21 12:03:40 +0330

    feat: add a new class base component and use it in the index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react"; // You also can do not import this module, it's will be import by compiler <--comment
 import ReactDOM from "react-dom";
+import App from "./app"; // Import personal component <--comment
 
-const element = <h1>Hello AI</h1>;
-ReactDOM.render(element, document.querySelector("#root"));
+// Start use locale element(jsx) writed <--comment
+
+// const element = <h1>Hello AI</h1>;
+// ReactDOM.render(element, document.querySelector("#root"));
+
+// End use locale element writed <--comment
+
+// Start use component <--comment
+ReactDOM.render(<App />, document.querySelector("#root"));
+// End use component <--comment
